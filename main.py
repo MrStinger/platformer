b49225022d91e22bc8ca597468e2afba79ce6378
MrStinger <arseny555eremin@gmail.com>
2025-06-01 12:36:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ class Platform(GameSprite):
 class Player(GameSprite):
     def __init__(self, img, x, y, w, h, speed):
         super().__init__(img, x, y, w, h, speed)   
-        self.opora = False   
-
-
+        self.opora = False  
 
     def update(self):
         keys = key.get_pressed()
@@ -31,6 +29,8 @@ class Player(GameSprite):
             self.rect.x-=self.speed
         if keys[K_RIGHT] and self.rect.x<700 -10- self.rect.width:
             self.rect.x+=self.speed 
+        if keys[K_SPACE] and self.opora:
+            self.rect.y += -150
 
     def gravity(self):
         self.opora = False
@@ -40,12 +40,54 @@ class Player(GameSprite):
         if not self.opora:
             self.rect.y += 1
 
+class Enemy(GameSprite):
+    def __init__(self, img, x, y, w, h, speed, fly=0):
+        super().__init__(img, x, y, w, h, speed)
+        self.fly = fly
+    def start(self, z1, z2):
+        self.z1 = z1
+        self.z2 = z2
+    def update(self):
+        if self.fly == 1:
+            if self.rect.y <= min(self.z1, self.z2):
+                self.direct = 1
+            elif self.rect.y >= max(self.z1, self.z2):
+                self.direct = -1
+            self.rect.y += self.speed*self.direct
+        else:
+            if self.rect.x <= min(self.z1, self.z2):
+                self.direct = 1
+            elif self.rect.x >= max(self.z1, self.z2):
+                self.direct = -1
+            self.rect.x += self.speed*self.direct
+
 
 
 window = display.set_mode((700,400))
 display.set_caption('Платформер')
 background = transform.scale(image.load('fon.png'), (700,400))
 player = Player('player.png', 10, 0, 50, 70, 5)
+
+enemyes = sprite.Group()
+enemy1 = Enemy('enemy.png', 200, 200, 50, 50, 1)
+enemy1.start(200,300)
+enemyes.add(enemy1)
+enemy2 = Enemy('enemy.png', 400, 200, 50, 50, 1, 1)
+enemy2.start(200,300)
+enemyes.add(enemy2)
+
+font.init()
+font1 = font.SysFont('Arial', 36)
+
+
+coins = sprite.Group()
+for i in range(2000):
+    x = randint(5, 695)
+    y= randint(200, 300)
+    coin = GameSprite('coin.png', x, y, 30, 35, 0)
+    coins.add(coin)
+
+
 
 platforms = sprite.Group()
 pl_count = 5
@@ -61,6 +103,7 @@ platforms.add(plt)
 game = True
 clock = time.Clock()
 fps = 60
+c_count = 0
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -71,6 +114,19 @@ while game:
     player.reset()
     player.update()
     player.gravity()
+
+    enemyes.update()
+    enemyes.draw(window)
+
+    coins.update()
+    coins.draw(window)
+
+    coin_count = font1.render('Coins:'+str(c_count), 1, (0,0,0))
+    window.blit(coin_count, (10,10))
+
+    spriteList = sprite.spritecollide(player, coins, True)
+    for i in spriteList:
+        c_count += 1
 
 
     clock.tick(fps)
